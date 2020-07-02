01eb85d708d4cef19b26ded0ad363876e0a2b75a
siragasnow <siraga613@gmail.com>
2020-07-02 16:48:57 +0900

    [finish]いいね機能・コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+  	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
 
 end
